ab66106b48379d75e8f33291d8fc14cd3fda0759
leonardvuniqi <vuniqileonard@outlook.com>
2021-06-22 22:11:13 -0400

    Fix query logic

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,9 +13,11 @@ class LoginController extends Controller
         if (Auth::attempt($request->only('email', 'password')))
         {
             $token = $request->user()->createToken('name');
+            $user = User::query()->where('email', $request->input('email'))->first();
             return response()->json([
                 "message" => "success",
-                "token" => $token->plainTextToken
+                "token" => $token->plainTextToken,
+                "user" => $user
             ], 200);
         }
 
